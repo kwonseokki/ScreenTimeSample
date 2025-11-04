233ca5be4b46b99a419aad5341480c1fb3724af4
권석기 <kwons3327@gmail.com>
2025-11-04 17:24:11 +0900

    Initial Commit

---
+++ ScreenTimeAPI/ScreenTimeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeAPIApp.swift
+//  ScreenTimeAPI
+//
+//  Created by a on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
